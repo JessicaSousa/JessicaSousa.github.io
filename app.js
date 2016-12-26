ec269082ef09c3325128f653054adc4f978c844f
JessicaSousa <jessicacardosops@gmail.com>
2016-12-26 00:20:21 -0200

    Update app.js

--- app.js
+++ app.js
@@ -81,11 +81,8 @@ function initMap() {
   
   var image = {
     url: 'images/home.png',
-    // This marker is 20 pixels wide by 32 pixels high.
     size: new google.maps.Size(50, 50),
-    // The origin for this image is (0, 0).
     origin: new google.maps.Point(0, 0),
-    // The anchor for this image is the base of the flagpole at (0, 32).
     anchor: new google.maps.Point(0, 50)
   };
 
